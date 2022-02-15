9ebac30ef5e15882b9b42d04e8898bb6d4d6bf43
Alex Badia <badia.alex.h@gmail.com>
2022-02-15 10:18:34 -0500

    Corrected nav links to match mock up, added donward chevrons for links that will open a sub menu

--- src/home/navbar/nav_button.tsx
+++ src/home/navbar/nav_button.tsx
@@ -1,19 +1,27 @@
 import { Link } from 'react-router-dom';
+import { ChevronDownIcon } from '@heroicons/react/solid'
 
+export interface LinkProps {
+  title: string,
+  reactRoute: string
+}
 export interface NavButtonProps {
   title: string;
-  links?: string[];
+  links?: LinkProps[];
 }
 
 
 export function NavButton({ title, links = [] }: NavButtonProps): JSX.Element {
   let reactLinks: JSX.Element[] = [];
   for (let link of links) {
-    reactLinks.push(<Link className="block px-4 py-3 text-center text-text-primary font-semibold" to={''}>{link}</Link>);
+    reactLinks.push(<Link className="block px-4 py-3 text-center text-xs text-text-primary" to={link.reactRoute}>{link.title}</Link>);
   }
   return (
     <div className="block group basis-1/12">
-      <button className="w-full px-4 py-2 text-text-primary font-semibold">{title}</button>
+      <button className="inline-flex justify-center items-center w-full px-4 py-2 text-text-primary font-semibold">
+        <span>{title}</span>
+        <ChevronDownIcon className="mr-1 ml-2 h-3 w-5" aria-hidden="true" />
+      </button>
       <div className="relative hidden group-hover:block -mt-1 z-10 bg-background-default">
         {reactLinks}
       </div>
