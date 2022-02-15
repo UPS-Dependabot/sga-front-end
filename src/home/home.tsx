c31d721f134802a9f4de00ebaee9f5dc3e51431a
Alex Badia <badia.alex.h@gmail.com>
2022-02-14 23:36:11 -0500

    Added title

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -9,9 +9,9 @@ import { Nav } from "./navbar/navbar";
 import { Box } from '@mui/system';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from "../themes";
-import { SgaStamp } from "./navbar/sga_stamp.";
+import { SgaStamp } from "./sga_stamp.";
 
-export function Home() {
+export function Home(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <Box
@@ -21,10 +21,20 @@ export function Home() {
           height: "100vh"
         }}>
         <SgaStamp />
+        <Title />
         <Nav />
         <SlideShowSection />
         <SocialMediaSection />
       </Box>
     </ThemeProvider>
+  );
+}
+
+export function Title(): JSX.Element {
+  return (
+    <div className="absolute w-full left-2/4 top-1/4 -translate-x-1/2 -translate-y-1/4 text-text-primary">
+      <span className="inline-block w-full text-7xl text-center tracking-wide font-semibold">Student Government Association</span>
+      <span className="inline-block w-full text-2xl text-center font-medium">Marist College</span>
+    </div>
   );
 }
--- src/home/sga_stamp..tsx
+++ src/home/sga_stamp..tsx
@@ -3,7 +3,7 @@ export function SgaStamp(): JSX.Element {
     <div className="absolute flex flex-row justify-center items-center left-10 top-5 w-[8rem] h-[8rem] rounded-full bg-white border-[1px] border-action-active">
       <div className="relative w-[7.7rem] h-[7.7rem] rounded-full bg-action-active z-10 flex flex-row justify-center items-center">
         <span className="text-center text-background-default text-xl font-medium">
-          <span className="block w-full tracking-wide pl-1">Marist</span>
+          <span className="block w-full tracking-wider pl-1">Marist</span>
           <span className="block w-full tracking-widest pl-4">SGA</span>
         </span>
       </div>
