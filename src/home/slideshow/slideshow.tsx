53f2359ee767ddeb11c90073d7854ea92a9ace93
Alex Badia <badia.alex.h@gmail.com>
2022-03-03 18:54:44 -0500

    Randomized start index in slideshow

--- src/home/slideshow/slideshow.tsx
+++ src/home/slideshow/slideshow.tsx
@@ -1,44 +1,32 @@
 /**
  * slideshow.ts
  * 
- * TODO: Implement slideshow, here ya go Gabe!
- * 
  * Takes images from the public/images/slideshow and fades them in and out
- * 
  */
  import {useState,useEffect} from 'react';
 
 export function SlideShowSection() {
   const secondsPerSlide = 5;
-  const maxOpacity = 70;
   
   const imageList = [
     "DesktopMCSGAFall2014.jpg",
-    "Marist SGA Background Dot.jpg",
-    "Marist SGA Background No Dot.jpg",
-    "Splash Logo.jpg"
+    "Marist-SGA-Background-Dot.jpg",
+    "Marist-SGA-Background-No-Dot.jpg",
+    "Splash-Logo.jpg"
   ]
-
-  const [ index , setIndex] = useState<number>(0); 
-  const [ opacity , setOpacity] = useState<number>(maxOpacity); 
+  const [ index , setIndex] = useState<number>(Math.floor(Math.random() * imageList.length)); 
 
   useEffect(() => {
     const timer = setInterval(() => {
-      let fadeOut = setInterval(fadeout,10);
-
-      function fadeout(){
-
-      }
-      setIndex((index + 1)%imageList.length);
-    }, secondsPerSlide*1000);
-               // clearing interval
+      setIndex((index + 1) % imageList.length);
+    }, secondsPerSlide * 1000);
     return () => clearInterval(timer);
   });
 
   return (
     <div className="flex flex-row w-full h-2/3 justify-center">
       <div className="flex flex-col justify-end h-full w-2/3 bg-background-paper text-center text-text-primary items-center">
-        {<img src={"images/slideshow/"+imageList[index]} className={"opacity-"+opacity+" max-w-fit h-full z-70"} alt="slideshow"></img>}
+        {<img src={`images/slideshow/${imageList[index]}`} className={"max-w-fit h-full z-70"} alt="slideshow"></img>}
         </div>
     </div>
   );
